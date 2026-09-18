c47d760ec919038695110cf3f625a1c4bc49de05
LearningGuides <clases.metodos2019@gmail.com>
2026-09-18 08:32:54 -0500

    lesson2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+
 import mdx from "@astrojs/mdx";
+import { unified } from "@astrojs/markdown-remark";
+
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
@@ -7,7 +10,9 @@ export default defineConfig({
   integrations: [mdx()],
 
   markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    processor: unified({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
   },
 });
